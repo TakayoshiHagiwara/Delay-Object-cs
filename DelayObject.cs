b78d2b288712be9c99666cbc6c34262f627c7528
TakayoshiHagiwara <takayoshi.hagiwara.18@gmail.com>
2022-04-15 16:01:11 +0900

    Initial commit

---
+++ DelayObject.cs
@@ -0,0 +1,127 @@
+// -------------------------------------------------- //
+// Author:      Takayoshi Hagiwara (Toyohashi University of Technology)
+// Created:     2019/8/27
+// Summary:     Apply delays to the movements of avatars, etc. The master and slave should be the same object structure.
+// Environment: Unity 2017 or Later
+// -------------------------------------------------- //
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayObject : MonoBehaviour {
+
+    [SerializeField, Tooltip("Objects that are the source of movement")]
+    private Transform masterRoot    = default;
+    [SerializeField, Tooltip("Objects that reflect movement with delay")]
+    private Transform slaveRoot     = default;
+
+    [SerializeField, Tooltip("Delay [s]")]
+    private float delay = 0;
+    private float elapsedTime;
+
+    // Temporarily save the Transform of a GameObject with the same name in a dictionary type.
+    private Dictionary<string, List<Vector3>> positionBuffer    = new Dictionary<string, List<Vector3>>();
+    private Dictionary<string, List<Quaternion>> rotationBuffer = new Dictionary<string, List<Quaternion>>();
+
+    bool isDelayStart = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        InitializeDictionary(masterRoot);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RecordParameter(masterRoot);
+
+        if (isDelayStart)
+            ApplyParameter(slaveRoot);
+        else
+            CheckCurrentTime();
+    }
+
+    /// <summary>
+    /// Temporarily save the master Transform.
+    /// </summary>
+    /// <param name="mst">master object</param>
+    public void RecordParameter(Transform mst)
+    {
+        if (positionBuffer.ContainsKey(mst.name))
+        {
+            positionBuffer[mst.name].Add(mst.position);
+            rotationBuffer[mst.name].Add(mst.rotation);
+        }
+
+        for(int iChild = 0; iChild < mst.childCount; iChild++)
+        {
+            RecordParameter(mst.GetChild(iChild));
+        }
+    }
+
+    /// <summary>
+    /// If the slave object contains a GameObject with the same name as the master,
+    /// the temporarily saved Transform is applied to the slave object.
+    /// </summary>
+    /// <param name="slv">slave object</param>
+    public void ApplyParameter(Transform slv)
+    {
+        if (rotationBuffer.ContainsKey(slv.name))
+        {
+            slv.position = positionBuffer[slv.name][0];
+            slv.rotation = rotationBuffer[slv.name][0];
+
+            positionBuffer[slv.name].RemoveAt(0);
+            rotationBuffer[slv.name].RemoveAt(0);
+        }
+
+        for(int iChild = 0; iChild < slv.childCount; iChild++)
+        {
+            ApplyParameter(slv.GetChild(iChild));
+        }
+    }
+
+    /// <summary>
+    /// Initialize dictionary for temporarily save the Transform of a GameObject.
+    /// Initialize with the structure and name of the master object.
+    /// </summary>
+    /// <param name="mst">master object</param>
+    private void InitializeDictionary(Transform mst)
+    {
+        positionBuffer.Clear();
+        rotationBuffer.Clear();
+
+        positionBuffer.Add(mst.name, new List<Vector3>());
+        rotationBuffer.Add(mst.name, new List<Quaternion>());
+
+        for(int iChild = 0; iChild < mst.childCount; iChild++)
+        {
+            InitializeDictionary(mst.GetChild(iChild));
+        }
+    }
+
+    /// <summary>
+    /// Check current time duration.
+    /// If elapsedTime exceeds the set delay time, the delay start flag is set to True.
+    /// </summary>
+    private void CheckCurrentTime()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime >= delay)
+            isDelayStart = true;
+    }
+
+    /// <summary>
+    /// Reset all parameters.
+    /// When you update the delay, please run this methods.
+    /// </summary>
+    /// <param name="mst">master object</param>
+    public void ResetAll(Transform mst)
+    {
+        InitializeDictionary(mst);
+        elapsedTime     = 0;
+        isDelayStart    = false;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # Delay-Object-cs
+<img src="https://img.shields.io/badge/Unity-2017 or Later-blue?&logo=Unity"> <img src="https://img.shields.io/badge/License-MIT-green">
+
+
+Unityで、あるGameObjectの動きを遅延させて、別のGameObjectに反映させるサンプルです。
+アバターなどの階層構造を持つGameObjectも対応可能です。
+
+# Environment
+- Unity 2017 or Later
+    - 2021での動作確認済み
+
+# Installation
+- 特に追加で必要なアセット、ライブラリはありません。
+
+# Usage
+
+1. Unityを開き、AssetsフォルダにDelayObject.csを読み込む
+1. Hierarchyに空のGameobjectを作成し、DelayObject.csをアタッチする
+1. InspectorのMaster Rootに動きの元になるオブジェクトを、Slave Rootに遅延させた動きを反映させたいオブジェクトをアタッチする (2つのオブジェクトの名前、階層構造は一致させる)
+1. Delayに遅延時間 (秒) を入力する
+1. 実行し、Masterに指定したオブジェクトを動かしたときに、遅れてSlaveに指定したオブジェクトが動く
+
+![InspectorDelayObject](https://user-images.githubusercontent.com/85339315/163533874-e1c4d4fd-0661-4497-bf94-cf70940ed9db.png)
+
+# Description
+- Dictionaryを用いてオブジェクトのTransformを一時保存し、別のオブジェクトに適用するという処理を行なっています。
+    - 実行時にMaster Rootに指定したオブジェクトの名前と階層構造をもとにDictionaryを初期化します。
+
+## DelayObject.cs
+### Parameters
+- masterRoot: Transform
+    - 動きのもとになるオブジェクトです。
+- slaveRoot: Transform
+    - 遅延した動きを反映するオブジェクトです。
+- delay: float
+    - 遅延時間です。単位は秒です。
+
+### Methods
+- RecordParameter
+    - MasterのTransformをDictionaryに一時保存します。
+- ApplyParameter
+    - 記録されたTransformを指定したオブジェクトに反映させます。
+- InitializeDictionary
+    - Dictionaryを初期化します。
+- CheckCurrentTime
+    - 実行時から現在までの経過時間を確認し、delayを超えた場合、SlaveにTransformを反映させるフラグをtrueにします。
+- ResetAll
+    - Dictionaryと経過時間を初期化します。
+    - 実行後に途中でdelayを変更した場合などに使用します。
+
+### Note
+- Master RootとSlave RootにアタッチするGameObjectは必ず同じ名前、同じ階層構造にしてください。
+- 外部から操作する場合は、外部で経過時間やトリガーを設定し、RecordParameter、ApplyParameterを呼び出してください。
+
+
+# Versions
+- 1.0: 2019/8/27
+- 1.1: 2022/4/15
+    - Unity 2021にて動作確認
+
+# Author
+- Takayoshi Hagiwara
+    - Graduate School of Media Design, Keio University
+    - Toyohashi University of Technology
+
+
+# License
+- MIT License
